2112d042ffa823724368d03b4c05df8f1472c88e
Dewan Mohammod Zahid Hasan <dewanmohammodzahidhasan@gmail.com>
2022-12-24 20:12:56 +0600

    finished

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../images/icon-check.svg";
 import "./Home.css";
 
 const Home = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <div className="card-container w-full">
-      <div className="header lg:w-1/2 w-full mx-auto p-5 lg:mb-0 mb-16 text-center">
-        <h1 className="lg:text-3xl text-2xl">Simple, traffic-based pricing</h1>
-        <p>Sign-up for our 30-day trial. No credit card required.</p>
-      </div>
-      <div className="card absolute mx-auto top-1/12 left-1/2 lg:mt-60 mt-48 lg:w-2/5 w-full  bg-base-100 shadow-xl">
-        <div className="card-body">
-          <div className="flex lg:flex-row flex-col card-header justify-between items-center">
-            <p className="font-bold">100K PAGE VIEWS</p>
-            <div className="my-5 w-full lg:hidden block">
+    <div className="min-h-screen dark:bg-[#293356] bg-[#fafbff]">
+      <div className="card-container dark:bg-[#cdd7ee] w-full">
+        <div className="flex justify-end">
+          <input
+            onClick={handleThemeSwitch}
+            type="checkbox"
+            className="toggle"
+          />
+        </div>
+        <div className="header lg:w-1/2 w-full mx-auto p-5 lg:mb-0 mb-16 text-center">
+          <h1 className="lg:text-3xl  text-2xl">
+            Simple, traffic-based pricing
+          </h1>
+          <p>Sign-up for our 30-day trial. No credit card required.</p>
+        </div>
+        <div className="card absolute top-1/12 left-1/2 lg:mt-60 mt-48 lg:w-2/5 md:w-2/5 w-4/5  bg-base-100 shadow-xl">
+          <div className="card-body">
+            <div className="flex lg:flex-row flex-col card-header justify-between items-center">
+              <p className="font-bold">100K PAGE VIEWS</p>
+              <div className="my-5 w-full lg:hidden block">
+                <input
+                  type="range"
+                  min="0"
+                  max="100"
+                  className="range range-accent"
+                />
+              </div>
+              <h2>
+                $16.00 <span className="text-sm">/month</span>
+              </h2>
+            </div>
+            {/* range start */}
+            <div className="my-5 lg:block hidden">
               <input
                 type="range"
                 min="0"
@@ -21,56 +58,44 @@ const Home = () => {
                 className="range range-accent"
               />
             </div>
-            <h2>
-              $16.00 <span className="text-sm">/month</span>
-            </h2>
-          </div>
-          {/* range start */}
-          <div className="my-5 lg:block hidden">
-            <input
-              type="range"
-              min="0"
-              max="100"
-              className="range range-accent"
-            />
-          </div>
-          {/* range end */}
-          <div className="flex justify-between items-center ">
-            <div className="flex items-center">
-              <p className="">Monthly Billing</p>
-              <input type="checkbox" className="toggle toggle-accent ml-3" />
-            </div>
-            <div>
-              <p>
-                Yearly Billing
-                <span className="bg-orange-100 p-1 ml-1 rounded-full">
-                  <span className="text-error text-xs">25%</span>
-                  <span className="text-error text-xs lg:inline-block hidden ">
-                    discount
+            {/* range end */}
+            <div className="flex justify-between items-center ">
+              <div className="flex items-center">
+                <p className="">Monthly Billing</p>
+                <input type="checkbox" className="toggle toggle-accent ml-3" />
+              </div>
+              <div>
+                <p>
+                  Yearly Billing
+                  <span className="bg-[#feece7] p-1 ml-1 rounded-full">
+                    <span className="text-[#ff8c66] text-xs">25%</span>
+                    <span className="text-error text-xs lg:inline-block hidden ">
+                      discount
+                    </span>
                   </span>
+                </p>
+              </div>
+            </div>
+            <hr className="my-5" />
+            <div className="card-actions flex lg:flex-row md:flex-row flex-col justify-between">
+              <div className="flex flex-col lg:mx-0 mx-auto">
+                <span className="text-xs my-3 flex">
+                  <img src={logo} alt="" />
+                  Unlimited website
                 </span>
-              </p>
+                <span className="text-xs my-3 flex">
+                  <img src={logo} alt="" />
+                  100% data ownership
+                </span>
+                <span className="text-xs my-3 flex">
+                  <img src={logo} alt="" />
+                  Email reports
+                </span>
+              </div>
+              <button className="btn btn-dark btn-sm lg:mx-0 mx-auto rounded-full">
+                Start my trial
+              </button>
             </div>
-          </div>
-          <hr className="my-5" />
-          <div className="card-actions flex lg:flex-row md:flex-row flex-col justify-between">
-            <div className="flex flex-col lg:mx-0 mx-auto">
-              <span className="text-xs my-3 flex">
-                <img src={logo} alt="" />
-                Unlimited website
-              </span>
-              <span className="text-xs my-3 flex">
-                <img src={logo} alt="" />
-                100% data ownership
-              </span>
-              <span className="text-xs my-3 flex">
-                <img src={logo} alt="" />
-                Email reports
-              </span>
-            </div>
-            <button className="btn btn-dark btn-sm lg:mx-0 mx-auto rounded-full">
-              Start my trial
-            </button>
           </div>
         </div>
       </div>
